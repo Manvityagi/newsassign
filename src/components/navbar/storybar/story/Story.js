a55fc238f8a64232d9f5b61bde4841ba891d935b
Uday Mittal <mittaluday435@gmail.com>
2021-08-15 13:18:34 +0530

    api

--- src/components/navbar/storybar/story/Story.js
+++ src/components/navbar/storybar/story/Story.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Story = () => {
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    const story = async () => {
+      const res = await axios.get('/appapi/lt/topstories');
+
+      setContent(res.data);
+      console.log(content);
+    };
+
+    story();
+  }, []);
+  console.log(content);
   return (
     <div>
       <img
